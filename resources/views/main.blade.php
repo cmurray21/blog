a163c7d97ff265c5865b58d8ec591f7381988ac0
Christopher Murray <christopher@christopher-murray.com>
2020-10-13 14:47:37 -0400

    Partial Blades

---
+++ resources/views/main.blade.php
@@ -0,0 +1,18 @@
+@include('partials._head')
+@include('partials._nav')
+
+  <div class="container">
+  @yield('content')
+  </div>
+<!-- end of .container -->
+@include('partials._js')
+@include('partials._footer')
+
+
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 | ** Best practice: Order from most specific to least specific, because Laravel will stop when there is a match.
 */
-Route::get('/contact', 'PagesController@getContact');
+Route::get('/contact', function () {
+    return view('contact');
+});
 
 Route::get('/about', function () {
+    $data = [];
     $first = "Christopher";
     $last = "Murray";
     $fullname = $first . " " . $last;
-
+    $email = "christopher@christopher-murray.com";
+    $data['email']=$email;
+    $data['fullname']=$fullname;
+    // These both do the same thing.
     // return view('about')-> with("fullname", $fullname);
-    return view('about')-> withFullname($fullname);
+    // Variables first and last are NOT accessible from view.
+    // return view('about')-> withFullname($fullname)->withEmail($email); 
+    // We can also pass in an array 
+    return view('about')->withData($data); 
 
 });
 
